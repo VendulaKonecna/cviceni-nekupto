ee3128996b630cb0f3491aa7a95e5b74b852a24f
Vendula Konečná <VendaKonecna@gmail.com>
2024-11-10 18:03:54 +0100

    fisrt commit

--- index.js
+++ index.js
@@ -15,3 +15,39 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+const product1HTML = `<img class="card-img-top" src=${product1.image} alt="Card image cap">
+        <div class="card-body">
+          <h2 class="card-title">${product1.name}</h2>
+          <p class="card-text">${product1.description}</p>
+        </div>
+        <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`
+const elProduct1 = document.querySelector("#product1")
+elProduct1.innerHTML = product1HTML
+
+const product2HTML = `<img class="card-img-top" src=${product2.image} alt="Slepičkabelka">
+        <div class="card-body">
+          <h2 class="card-title">${product2.name}</h2>
+          <p class="card-text">${product2.description}</p>
+        </div>
+        <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`
+const elProduct2 = document.querySelector("#product2")
+elProduct2.innerHTML = product2HTML
+
+const product3HTML = `<img class="card-img-top" src=${product3.image} alt="Anatoplavky">
+        <div class="card-body">
+          <h2 class="card-title">${product3.name}</h2>
+          <p class="card-text">${product3.description} </p>
+        </div>
+        <button type="button" class="btn btn-lg btn-primary btn-block">Koupit</button>`
+const elProduct3 = document.querySelector("#product3")
+elProduct3.innerHTML = product3HTML
+
+const firstCard = document.querySelector("#product1")
+firstCard.classList.add("border-primary")
+
+const secondCardButton = document.querySelector("#product2 button")
+secondCardButton.classList.remove("btn-primary")
+
+const thirdCardTitle = document.querySelector('#product3 .card-title');
+thirdCardTitle.classList.toggle('text-center');
